5e802345ff23adb0728a5af4b887ddcc42d75154
BlueGlassBlock <blueglassblock@outlook.com>
2022-12-06 16:18:54 +0800

    feat: PlumbingClient.other_clients

--- python/ichika/ichika.pyi
+++ python/ichika/ichika.pyi
@@ -1,4 +1,5 @@
 import datetime
+from dataclasses import dataclass
 from types import ModuleType
 from typing import Callable, TypedDict
 
@@ -53,6 +54,24 @@ class Account:
     event_callbacks: list[Callable[[Any], Any]]
     def __init__(self, uin: int, data_folder: str, protocol: str) -> None: ...  # TODO: Literal
     async def login(self, method: _LoginMethodTransfer) -> Client: ...
+
+class PlumbingClient:
+    async def keep_alive(self) -> None: ...
+    @property
+    def online(self) -> bool: ...
+    @dataclass(frozen=True)
+    class __AccountInfo:
+        nickname: str
+        age: int
+        gender: int
+    async def account_info(self) -> __AccountInfo: ...
+    @dataclass(frozen=True)
+    class __OtherClientInfo:
+        app_id: int
+        instance_id: int
+        sub_platform: str
+        device_kind: str
+    async def other_clients(self) -> list[__OtherClientInfo]: ...
 
 def face_id_from_name(name: str) -> int | None: ...
 def face_name_from_id(id: int) -> str: ...
--- src/client/structs.rs
+++ src/client/structs.rs
@@ -9,3 +9,14 @@ pub struct AccountInfo {
     #[pyo3(get)]
     pub gender: u8,
 }
+#[pyclass(module = "ichika.client.structs#rs")]
+pub struct OtherClientInfo {
+    #[pyo3(get)]
+    pub app_id: i64,
+    #[pyo3(get)]
+    pub instance_id: i32,
+    #[pyo3(get)]
+    pub sub_platform: String,
+    #[pyo3(get)]
+    pub device_kind: String,
+}
