08fbed976914c4fdfe3a4c093c53615191fab2a6
BlueGlassBlock <blueglassblock@outlook.com>
2022-12-15 12:35:52 +0800

    ci: fix script on windows

---
+++ .github/build-helper.py
@@ -0,0 +1,20 @@
+import sys
+from typing import Any
+
+import tomlkit
+
+sha, release, gh_env = sys.argv[1:]
+
+release = release == "true"
+if not release:
+    with open("./Cargo.toml") as cargo_file:
+        doc: Any = tomlkit.load(cargo_file)
+
+    doc["package"]["version"] = doc["package"]["version"] + "-dev." + sha[:7]
+
+    with open("./Cargo.toml", "w") as cargo_file:
+        tomlkit.dump(doc, cargo_file)
+
+with open(gh_env, "a") as env:
+    env.write("BASE_BUILD='--release --out --dist'" if release else "BASE_BUILD='--out --dist'")
+    env.write("\n")
