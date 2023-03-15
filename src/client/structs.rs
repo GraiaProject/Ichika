f7c7571becac950663b1cc8a8a4ec18f81c74915
BlueGlassBlock <blueglassblock@outlook.com>
2023-03-15 14:12:52 +0800

    feat: group essence & online status See #17

--- src/client/structs.rs
+++ src/client/structs.rs
@@ -27,3 +27,32 @@ pub struct RawMessageReceipt {
     pub kind: String,
     pub target: i64,
 }
+
+#[derive(FromPyObject)]
+pub enum OnlineStatusParam {
+    #[pyo3(annotation = "tuple[bool, int]")]
+    Normal(bool, u64),
+    #[pyo3(annotation = "tuple[int, str]")]
+    Custom(u64, String),
+}
+
+impl From<OnlineStatusParam> for ricq::structs::Status {
+    fn from(value: OnlineStatusParam) -> Self {
+        use ricq::structs::{CustomOnlineStatus, Status};
+        match value {
+            OnlineStatusParam::Custom(face_index, wording) => Status {
+                online_status: 11,
+                ext_online_status: 2000,
+                custom_status: Some(CustomOnlineStatus {
+                    face_index,
+                    wording,
+                }),
+            },
+            OnlineStatusParam::Normal(is_ext, index) => Status {
+                online_status: if is_ext { 11 } else { index as i32 },
+                ext_online_status: if is_ext { index as i64 } else { 0 },
+                custom_status: None,
+            },
+        }
+    }
+}
