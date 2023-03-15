2cf08a07905057dc79a459f7351ef19c7c8911e4
BlueGlassBlock <blueglassblock@outlook.com>
2023-03-15 18:19:55 +0800

    chore(changelog): add dangling 0.0.3 changes

--- .github/scripts/extract_version.py
+++ .github/scripts/extract_version.py
@@ -5,4 +5,4 @@ from actions_toolkit import core
 
 with open("./Cargo.toml") as cargo_file:
     doc: Any = tomlkit.load(cargo_file)
-    core.set_output("version", str(doc["package"]["version"]))
+    core.export_variable("VERSION", str(doc["package"]["version"]))
